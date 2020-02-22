7e0f8e9e6d7f27001f02d3c7bc775480fc30fb40
ap10001 <61152191+ap10001@users.noreply.github.com>
2020-02-23 03:39:29 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,29 +6,64 @@ rm -rf /usr/bin/v2ray/geosite.dat /usr/bin/v2ray/geoip.dat
 # V2Ray new configuration
 cat <<-EOF > /etc/v2ray/config.json
 {
-  "inbounds": [
-  {
-    "port": ${PORT},
-    "protocol": "vmess",
-    "settings": {
-      "clients": [
-        {
-          "id": "${UUID}",
-          "alterId": 64
-        }
-      ]
+    "policy":null,
+    "log":{
+        "access":"",
+        "error":"",
+        "loglevel":"warning"
     },
-    "streamSettings": {
-      "network": "ws"
+    "inbounds":[
+        {
+            "tag":null,
+            "port":${PORT},
+            "listen":null,
+            "protocol":"vmess",
+            "sniffing":null,
+            "settings":{
+                "auth":null,
+                "udp":false,
+                "ip":null,
+                "address":null,
+                "clients":[
+                    {
+                        "id":"${UUID}",
+                        "alterId":64,
+                        "email":"t@t.tt",
+                        "security":null
+                    }
+                ]
+            },
+            "streamSettings":{
+                "network":"ws",
+                "security":"tls",
+                "tlsSettings":{
+                    "allowInsecure":true,
+                    "serverName":"${APPNAME}.herokuapp.com"
+                },
+                "tcpSettings":null,
+                "kcpSettings":null,
+                "wsSettings":{
+                    "connectionReuse":true,
+                    "path":"${PATH}",
+                    "headers":{
+                        "Host":"${APPNAME}.herokuapp.com"
+                    }
+                },
+                "httpSettings":null,
+                "quicSettings":null
+            }
+        }
+    ],
+    "outbounds":null,
+    "stats":null,
+    "api":null,
+    "dns":null,
+    "routing":{
+        "domainStrategy":"IPIfNonMatch",
+        "rules":[
+
+        ]
     }
-  }
-  ],
-  "outbounds": [
-  {
-    "protocol": "freedom",
-    "settings": {}
-  }
-  ]
 }
 EOF
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
